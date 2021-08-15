3729bf0bdc99a08b354578c4858796ecd195cf32
yeslol234 <72095827+yeslol234@users.noreply.github.com>
2021-08-15 11:52:10 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
 using namespace std;
 int main() {
+  // VARIABLES
+   int tempf, tempc;
+    int mode;
+ cout << "Enter a Mode";
+  cin >> mode;
+  switch (mode) {
+    case 1:
 
+    break;
+      
+    case 2:
+      
+    break;
+      
+    default:
+
+      break;
+  }
 }
